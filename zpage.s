26048230691f956a3adc9927c4981408e83237ac
Joel Heikkila <jjheik@gmail.com>
2014-02-22 01:43:33 -0500

    added zpage.s

---
+++ zpage.s
@@ -0,0 +1,49 @@
+.module zpage
+
+.globl	_isr
+.globl	_reset
+
+.area	_CODE (REL)
+
+.globl _zpage
+
+_zpage:
+	jp _reset ; 3 bytes
+
+.blkb 0x1d	; 0x1d + 3 = 0x20
+
+; rst 0x20
+	ret
+	
+	.db 0
+	.db 0
+	.db 0
+
+	.db 0
+	.db 0
+	.db 0
+	.db 0
+; rst 0x28
+	.db 0
+	.db 0
+	.db 0
+	.db 0
+
+	.db 0 
+	.db 0
+	.db 0
+	.db 0
+; rst 0x30
+	.db 0
+	.db 0
+	.db 0
+	.db 0
+
+	.db 0
+	.db 0
+	.db 0
+	.db 0
+; rst 0x38
+	call _isr
+	ei
+	ret
