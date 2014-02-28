573bf9b49d275d25b03cf45582b922528626c2f0
Joel Heikkila <jjheik@gmail.com>
2014-02-28 15:55:17 -0500

    finalized (hopefully) inode and superblock layout

--- include/fs.h
+++ include/fs.h
@@ -6,12 +6,12 @@
 
 struct superblock {
 	uint16_t	magic;		// 0xdead
-	uint32_t	nblocks;	// number of blocks
-	uint32_t	ninodes;	// number of inodes
 	uint16_t	fs_version;	// version 1
 
-	uint32_t	mtime;		// last time FS was mounted
-	uint32_t	ctime;		// time allowed since last check
+
+	uint32_t	nblocks;	// number of blocks
+	uint32_t	ninodes;	// number of inodes
+
 	uint32_t	mcount;		// times mounted since last check
 	uint32_t	mmax;		// maximum times to be mounted before
 							// check is forced
@@ -24,21 +24,24 @@ struct superblock {
 };
 
 struct inode {
-	uint8_t		type;
+	uint16_t	mode;
+	
+	uint16_t	nlinks;
+
 	uint16_t	major;
 	uint16_t	minor;
-	uint16_t	nlink;
+	
 	uint16_t	uid;
 	uint16_t	gid;
+	
 	uint32_t	size;
 
 	uint32_t	atime;
 	uint32_t	mtime;
 	uint32_t	ctime;
 
-	uint16_t	pad;
-
-	uint32_t	direct[];
+	uint32_t	direct[6];
 	uint32_t	indirect;
 	uint32_t	dbindirect;
+	uint32_t	triplyindirect;
 }
